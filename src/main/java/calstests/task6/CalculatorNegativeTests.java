8a23f27e775f061b8316ed16ffd6de6682a5c3fd
eexmr42 <Lakshmi.Arivelu@McKesson.com>
2024-07-07 13:37:03 +0530

    updated task 7

--- src/main/java/calstests/task6/CalculatorNegativeTests.java
+++ src/main/java/calstests/task6/CalculatorNegativeTests.java
@@ -3,27 +3,21 @@ package calstests.task6;
 import calstests.listeners.Retry;
 import com.epam.tat.module4.Calculator;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class CalculatorParameterisedTest {
+public class CalculatorNegativeTests {
     Calculator calculator = new Calculator();
 
 
     @Test
     @Parameters({"num1","num2"})
-    public void testAddition2(double num1, double num2){
-        double res = calculator.sum(num1,num2);
-        Assert.assertEquals(res,num1+num2,"Addition operation failed");
+    public void testAddition1(double num1, double num2){
+        double res = calculator.sum(-num1,-num2);
+        Assert.assertEquals(res,-num1-num2,"Addition operation failed");
     }
 
-    @Test
-    @Parameters({"num1","num2"})
-    public void testSubtraction2(double num1, double num2) {
-        double result = calculator.sub(num1, num2);
-        Assert.assertEquals(result, num1-num2, "Subtraction operation failed");
-    }
+
 
     @Test
     @Parameters({"num1","num2"})
@@ -53,12 +47,25 @@ public class CalculatorParameterisedTest {
         Assert.assertEquals(result, num1/num2, "multiplication operation failed");
     }
 
-    @Test(dataProvider = "getadditiondata", dataProviderClass = DataProviderClass.class)
-    public void testdataprovider(double num1, double num2, double res){
-        double actRes = calculator.sum(num1,num2);
-       // System.out.println("testdataprovider:"+actRes);
-        Assert.assertEquals(actRes,res,"Addition operation failed");
+    @Test(expectedExceptions = NumberFormatException.class)
+    @Parameters({"num1", "num2"})
+    public void testDivision2(String num1Str, String num2Str) {
+        double num1 = Double.parseDouble(num1Str);
+        double num2;
+        try {
+            num2 = Double.parseDouble(num2Str);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException();
+        }
+        double result = calculator.div(num1, num2);
+        Assert.assertEquals(result, num1 / num2, "division operation failed");
     }
 
+    @Test
+    @Parameters({"num1", "num2"})
+    public void testDivision3(double num1, double num2) {
+            double result = calculator.div(num1, -num2);
+            Assert.assertEquals(result, -num1 / num2, "division operation failed");
+    }
 
 }
