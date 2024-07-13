768107d2d539b6cee351e8c75999442d041ef73b
Lakshmi Arivelu <lakshmi_arivelu@epam.com>
2024-07-13 00:13:21 +0000

    added param

--- src/main/java/com/epam/HelloParam.bat
+++ src/main/java/com/epam/HelloParam.bat
@@ -1,10 +1,4 @@
 @echo off
 
-rem Check if the parameter is provided
-if "%~1" == "" (
-    echo Usage: %0 [name]
-    exit /b 1
-)
-
-rem Use the parameter passed to the script
-echo Hello %1
+echo 'Hello, %name%'
+pause
